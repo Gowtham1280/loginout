286f7f54fd45731959d0ea288e4b1d3847cf0613
Chikilammetla <924612@cognizant.com>
2022-11-01 11:27:11 +0530

    email login-logout

---
+++ src/store/auth-context.js
@@ -0,0 +1,37 @@
+import React, { useState,useEffect } from "react";
+
+const AuthContext=React.createContext({
+    isLoggedIn:false,
+    onLogout: () =>{},
+    onLogin: ()=>{}
+});
+
+export const AuthContextProvider =(props)=>{
+
+    const [isLoggedIn,setIsLoggedIn]=useState(false);
+
+    useEffect(()=>{
+        const storedinfo=localStorage.getItem('IsLoggedIn');
+        if(storedinfo==="1"){
+          setIsLoggedIn(true);
+        }
+    
+      },[])
+    
+
+    const logoutHandler=()=>{
+        localStorage.removeItem('IsLoggedIn');
+        setIsLoggedIn(false);
+    };
+
+    const loginHandler=()=>{
+        localStorage.setItem('IsLoggedIn','1');
+        setIsLoggedIn(true);
+    }
+
+   return <AuthContext.Provider value={{isLoggedIn:isLoggedIn , onLogout:logoutHandler,onLogin:loginHandler}}>
+     {props.children}</AuthContext.Provider>;
+
+}
+
+export default AuthContext;
